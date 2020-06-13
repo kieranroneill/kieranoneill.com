ee196de909c55714e42dfb2b3d9a46a9f1b013ac
Kieran O'Neill <kieran.oneill@protonmail.com>
2020-06-13 19:51:40 +0100

    Adds hello human

---
+++ webpack/webpack.config.ts
@@ -0,0 +1,57 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { resolve } from 'path';
+import { Configuration, DefinePlugin } from 'webpack';
+import merge from 'webpack-merge';
+
+// Config.
+import commonConfig from './common.config';
+
+// Constants.
+import * as WebpackConstants from './constants';
+
+const config: Partial<Configuration> = merge(commonConfig, {
+  devtool: 'source-map',
+
+  mode: 'production',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          // Rule of thumb: add any vendor files that are > 50kb
+          chunks: 'initial',
+          enforce: true,
+          name: 'vendor',
+          test: /react|react-dom/,
+        },
+      },
+    },
+  },
+
+  output: {
+    chunkFilename: '[name].[chunkhash].js',
+    filename: '[name].[hash].js',
+    path: WebpackConstants.DIST_PATH,
+  },
+
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true,
+      },
+      template: resolve(WebpackConstants.SRC_PATH, 'index.hbs'),
+      title: WebpackConstants.TITLE,
+    }),
+  ],
+});
+
+export default config;
---
+++ webpack/webpack.dev.config.ts
@@ -0,0 +1,62 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { resolve } from 'path';
+import {
+  Configuration,
+  DefinePlugin,
+  HotModuleReplacementPlugin,
+} from 'webpack';
+import merge from 'webpack-merge';
+import WebpackNotifierPlugin from 'webpack-notifier';
+
+// Config.
+import commonConfig from './common.config';
+
+// Constants.
+import * as WebpackConstants from './constants';
+
+const config: Configuration = merge(commonConfig, {
+  devServer: {
+    contentBase: WebpackConstants.DIST_PATH,
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: WebpackConstants.PORT,
+  },
+
+  devtool: 'cheap-module-source-map',
+
+  entry: [
+    `webpack-dev-server/client?${WebpackConstants.LOCALHOST}:${WebpackConstants.PORT}`,
+    'webpack/hot/only-dev-server',
+    resolve(WebpackConstants.SRC_PATH, 'index.ts'),
+  ],
+
+  mode: 'development',
+
+  output: {
+    filename: 'bundle.js',
+    path: WebpackConstants.DIST_PATH,
+    publicPath: '/',
+  },
+
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+      },
+    }),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      minify: false,
+      template: resolve(WebpackConstants.SRC_PATH, 'index.hbs'),
+      title: WebpackConstants.TITLE,
+    }),
+    new HotModuleReplacementPlugin(),
+    new WebpackNotifierPlugin({
+      alwaysNotify: true,
+      contentImage: resolve(__dirname, 'unicorn.png'),
+      title: 'UNICORN POWER_UP!!!',
+    }),
+  ],
+});
+
+export default config;
