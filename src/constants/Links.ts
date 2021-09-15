6bb765b3247e8579a6f0d8ee72da045604e87fc0
Kieran O'Neill <kieran.oneill@protonmail.com>
2021-09-15 12:49:58 +0200

    feat: add blog link (#37)
    * feat: add blog link to mobile site
    
    * feat: add blog link to desktop terminal

--- src/constants/Links.ts
+++ src/constants/Links.ts
@@ -1,3 +1,4 @@
+export const BLOG: string = 'https://fromthecornerofakitchentable.blog';
 export const GITHUB: string = 'https://github.com/kieranroneill';
 export const LINKEDIN: string = 'https://www.linkedin.com/in/kieranroneill';
 export const TWITTER: string = 'https://twitter.com/kieranroneill';
